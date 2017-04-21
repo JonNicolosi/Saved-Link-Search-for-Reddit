26b13c09649d77c0919fd230bd75e4bb209ef67d
Jon Nicolosi <jonicolosi@gmail.com>
2017-04-21 17:04:01 -0400

    fixed tabulation issue

--- main.js
+++ main.js
@@ -16,12 +16,13 @@ function grabAll(count, after){
 			var row = table.insertRow(table.rows.length);
 			var cell1 = row.insertCell(0);
 			var cell2 = row.insertCell(1);
+
 			var link = document.createElement('a')
-			document.getElementById("myTableBody").childNodes[i+1].childNodes[0].appendChild(link);
-			document.getElementById("myTableBody").childNodes[i+1].childNodes[0].childNodes[0].setAttribute("href", URL);
+			document.getElementById("myTableBody").childNodes[i+1+(count-25)].childNodes[0].appendChild(link);
+			document.getElementById("myTableBody").childNodes[i+1+(count-25)].childNodes[0].childNodes[0].setAttribute("href", URL);
 			var img = document.createElement('img');
 			img.src= result.data.children[i].data.thumbnail;
-			document.getElementById("myTableBody").childNodes[i+1].childNodes[0].childNodes[0].appendChild(img);
+			document.getElementById("myTableBody").childNodes[i+1+(count-25)].childNodes[0].childNodes[0].appendChild(img);
 
 			if(result.data.children[i].kind == "t3"){
 				cell2.innerHTML = result.data.children[i].data.title;
@@ -32,9 +33,9 @@ function grabAll(count, after){
 		}
 
 
-		// if(result.data.after!=null){
-		// 	grabAll(count+=25,result.data.after);
-		// }
+		if(result.data.after!=null){
+			grabAll(count+=25,result.data.after);
+		}
 	}});
 }
 
