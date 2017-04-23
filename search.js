5dc31648ef7857b5bf2626be4b55425043eb3dba
Jon Nicolosi <jonicolosi@gmail.com>
2017-04-23 18:56:56 -0400

    Title search fully functional

--- search.js
+++ search.js
@@ -1,5 +1,17 @@
 document.getElementById("myInput").addEventListener("keyup", function(){
 
+for(var i = 1; i < document.getElementById("myTableBody").childNodes.length; i++){
 
+  var input = document.getElementById("myInput").value.toUpperCase();
+  var text_to_search = document.getElementById("myTableBody").childNodes[i].childNodes[1].innerText.toUpperCase();
+
+  if(text_to_search.search(input) == -1){
+    document.getElementById("myTableBody").childNodes[i].style.display = "none";
+  }
+  else{
+    document.getElementById("myTableBody").childNodes[i].style.display = "";
+  }
+
+}
 
 });
