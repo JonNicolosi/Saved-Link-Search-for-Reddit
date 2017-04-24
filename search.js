c22e4f93e99a390167645f577c0cb72c90a14bea
Jon Nicolosi <jonicolosi@gmail.com>
2017-04-23 23:20:43 -0400

    Replaced dual search with radio buttons

--- search.js
+++ search.js
@@ -1,17 +1,28 @@
+var input = document.getElementById("myInput").value.toUpperCase();
+
 document.getElementById("myInput").addEventListener("keyup", function(){
 
-for(var i = 1; i < document.getElementById("myTableBody").childNodes.length; i++){
+if(document.getElementById("bytitlecommentbody").childNodes[0].checked || document.getElementById("bysubreddit").childNodes[0].checked){
+  for(var i = 1; i < document.getElementById("myTableBody").childNodes.length; i++){
 
-  var input = document.getElementById("myInput").value.toUpperCase();
-  var text_to_search = document.getElementById("myTableBody").childNodes[i].childNodes[1].innerText.toUpperCase();
+    input = document.getElementById("myInput").value.toUpperCase();
+    if(document.getElementById("bytitlecommentbody").childNodes[0].checked){
+      var text_to_search = document.getElementById("myTableBody").childNodes[i].childNodes[1].innerText.toUpperCase();
+    }
+    else{
+      var text_to_search = document.getElementById("myTableBody").childNodes[i].childNodes[2].innerText.toUpperCase();
+    }
 
-  if(text_to_search.search(input) == -1){
-    document.getElementById("myTableBody").childNodes[i].style.display = "none";
+
+    if(text_to_search.search(input) == -1){
+
+      document.getElementById("myTableBody").childNodes[i].style.display = "none";
+    }
+    else{
+
+      document.getElementById("myTableBody").childNodes[i].style.display = "";
+    }
   }
-  else{
-    document.getElementById("myTableBody").childNodes[i].style.display = "";
-  }
-
 }
 
 });
